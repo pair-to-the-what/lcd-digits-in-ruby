051b637155da5d80dd17cd7c0c38411401f5d650
Michael <mjg.py3@gmail.com>
2015-03-15 18:44:06 -0400

    First red green...

--- lcd_digits.rb
+++ lcd_digits.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
 
-puts "Almost empty Ruby file..."
+require 'test/unit'
+
+class TestLcdDigits < Test::Unit::TestCase
+  def test_given_one_it_works
+    result = lcd_digits(1)
+
+    expected = "...\n..|\n..|"
+
+    assert_equal(expected, result)
+  end
+end
+
+def lcd_digits(_)
+  "...\n..|\n..|"
+end
