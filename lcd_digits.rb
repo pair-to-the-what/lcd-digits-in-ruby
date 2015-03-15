8808d746e308043b857450d2a0ad5d8e093b6d27
Michael <mjg.py3@gmail.com>
2015-03-15 19:04:04 -0400

    [Green] made 7 work

--- lcd_digits.rb
+++ lcd_digits.rb
@@ -7,23 +7,24 @@ class TestLcdDigits < Test::Unit::TestCase
     result = 1.to_lcd
 
     expected = "...\n..|\n..|"
-	
+
     assert_equal(expected, result)
 
   end
+
   def test_given_seven_it_works
     result = 7.to_lcd
 
     expected = "._.\n..|\n..|"
-	
+
     assert_equal(expected, result)
-
   end
-
 end
 
 class Fixnum
   def to_lcd
+    return "._.\n..|\n..|" if self == 7
+
     "...\n..|\n..|"
   end
 end
