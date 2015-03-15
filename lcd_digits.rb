4662346aa0b2d7e1f69641ed96a4587133cffa4d
Michael <mjg.py3@gmail.com>
2015-03-15 19:32:11 -0400

    [Green] Made 5 work

--- lcd_digits.rb
+++ lcd_digits.rb
@@ -7,7 +7,7 @@ class TestLcdDigits < Test::Unit::TestCase
     1 => "...\n..|\n..|",
     7 => "._.\n..|\n..|",
     0 => "._.\n|.|\n|_|",
-	5 => "._.\n|_.\n._|"
+    5 => "._.\n|_.\n._|"
   }.each do |given_number, expected_digits|
     define_method("test_given_#{given_number}_it_works") do
       assert_equal(expected_digits, given_number.to_lcd)
@@ -19,6 +19,7 @@ class Fixnum
   def to_lcd
     return "._.\n..|\n..|" if self == 7
     return "._.\n|.|\n|_|" if self == 0
+    return "._.\n|_.\n._|" if self == 5
 
     "...\n..|\n..|"
   end
